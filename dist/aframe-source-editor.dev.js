f08795666cd78f475a6a2a76ca280b93c99fe5b4
Seth Nickell <snickell@gmail.com>
2017-11-20 18:18:05 -0800

    a-source-editor works

--- dist/aframe-source-editor.dev.js
+++ dist/aframe-source-editor.dev.js
@@ -28659,29 +28659,29 @@ return;
 ;
 console.log("Registering a-source-editor!");
 
-var saveEvent = new Event('build');
-
 AFRAME.registerComponent("source-editor", {
   schema: {
     value: { type: 'string' },
 		title: { type: 'string' },
 		target: { type: 'string' },
+		mode: { type: 'string', default: 'javascript' },
 		'pixel-width': { type: 'int', default: 2048},
-		'pixel-height': { type: 'int', default: 1024}
+		'pixel-height': { type: 'int', default: 1024},
+		'save-button': { type: 'boolean', default: false}
   },
   init: function () {
     const codeEditor = new THREE.CodeEditor({
       vr: true,
       events: this.el
     });
+		
     this.codeEditor = codeEditor;
-    window.aceEditor = codeEditor.aceEditor;
     
     codeEditor.aceEditor.commands.addCommands(THREE.CodeEditor.commands.javascript);
 
     ace.config.set("basePath", "https://unpkg.com/aframe-source-editor@0.1.1/vendor/ace/");
     ace.config.set("modePath", "https://unpkg.com/aframe-source-editor@0.1.1/vendor/ace/");
-    codeEditor.setSize(this.data.width, this.data.height);
+    codeEditor.setSize(this.data['pixel-width'], this.data['pixel-height']);
     codeEditor.aceEditor.setTheme("ace/theme/chrome");
 
     // FIXME setKeyboardHandler() is broken, it won't load the right amd module
@@ -28699,7 +28699,7 @@ AFRAME.registerComponent("source-editor", {
     });
 
     THREE.CodeEditor.autocomplete.installDynamicJSCompleterInto(codeEditor.aceEditor);
-    codeEditor.aceEditor.setOption("mode", "ace/mode/html");
+    codeEditor.aceEditor.setOption("mode", "ace/mode/javascript");
     
 
     // FIXME: this makes the ace editor text disappear from the canvas... why?!?
@@ -28707,8 +28707,8 @@ AFRAME.registerComponent("source-editor", {
 
     codeEditor.scale.x = 0.0025;
     codeEditor.scale.y = 0.0025;
-    codeEditor.scale.z = 0.0025;
-
+    codeEditor.scale.z = 0.0025;		
+				
     this.el.setObject3D('codeeditor', codeEditor);
   },
   setText: function (text) {
@@ -28720,18 +28720,14 @@ AFRAME.registerComponent("source-editor", {
 	getAceEditor: function () {
 		return this.codeEditor.aceEditor;
 	},
-	init: function () {
-    const saveButton = document.createElement("a-button");
-    saveButton.setAttribute("value", "Save");
-    saveButton.addEventListener("click", this.saveClicked.bind(this));
-    saveButton.setAttribute("position", "-2.5 -1.1 0");
-    saveButton.setAttribute("button-color", "#ff69b4");
-    saveButton.setAttribute("scale", "0.7 0.7 0.7");
-    this.el.appendChild(saveButton);		
-	},
 	saveClicked: function () {
-		console.log("saveClicked");
 		if (this.target) this.saveToTarget();
+		var saveEvent = new CustomEvent('save', { 
+			detail: { 
+				text: this.getText(), 
+				target: this.target 
+			}
+		});
   	this.el.dispatchEvent(saveEvent);
   },
   update: function () {
@@ -28743,14 +28739,28 @@ AFRAME.registerComponent("source-editor", {
       title.setAttribute("scale", "0.5 0.5 0.5")
       this.el.appendChild(title);				
 		}
+		
+		if (this.data['save-button']) {
+			console.log("Creating a save button");
+	    const saveButton = document.createElement("a-button");
+	    saveButton.setAttribute("value", "Save");
+	    saveButton.addEventListener("click", this.saveClicked.bind(this));
+	    saveButton.setAttribute("position", "-2.5 -1.1 0");
+	    saveButton.setAttribute("button-color", "#ff69b4");
+	    saveButton.setAttribute("scale", "0.7 0.7 0.7");
+	    this.el.appendChild(saveButton);			
+		}
 				
     if (this.data.value) {
       this.setText(this.data.value);
     } else if (this.data.target) {
       this.target = document.getElementById(this.data.target);
       this.updateFromTarget();
-      this.codeEditor.aceEditor.setOption("mode", "ace/mode/javascript");
     }
+		
+		if (this.data.mode) {
+			this.getAceEditor().setOption("mode", "ace/mode/" + this.data.mode);
+		}
   },
   updateFromTarget: function () {
     this.setText(this.target.innerText);
@@ -28769,6 +28779,8 @@ AFRAME.registerPrimitive("a-source-editor", {
   mappings: {
     target: 'source-editor.target',
     value: 'source-editor.value',
+		mode: 'source-editor.mode',
+		'save-button': 'source-editor.save-button',
 		'pixel-width': 'source-editor.pixel-width',
 		'pixel-height': 'source-editor.pixel-height'
   }
